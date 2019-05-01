6434f1421235644a00988c361a92c12f2ae030bf
carlton anand <carlton.anand@gmail.com>
2019-05-01 13:12:39 +0100

    made my own reposity master adding changes in docking_station.rb spec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 
 # In spec/docking_station_spec.rb
 describe DockingStation do
-  it release_ bike do
+  it release_bike do
    expect(DockingStation).to respond_to(release_bike)
   end
 end
