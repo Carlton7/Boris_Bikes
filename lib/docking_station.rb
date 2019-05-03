8192b8d648f5850e889bdd64fd8f4b2e10d70c8a
carlton anand <carlton.anand@gmail.com>
2019-05-03 17:26:25 +0100

    adding updates to docking station spec and lib files

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,28 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize() 
+    @bike_array = []
+    @DEAFAULT_CAPACITY = 20
+    # @capacity = capacity
+  end
   def release_bike
-    fail 'no availble bikes' unless @bike
-    @bike
+    fail 'no availble bikes' if empty?
+    Bike.new
   end
   def dock(bike)
-    fail 'docking station capacity exceeded' if @bike
-    @bike = bike
+    fail 'docking station capacity exceeded' if full?
+    @bike_array << bike 
   end
-  attr_reader :bike
+  attr_reader :bike_array, :DEAFAULT_CAPACITY
+
+
+  private
+  def full?
+    @bike_array.length >= @DEAFAULT_CAPACITY ? true : false
+  end
+  def empty?
+    @bike_array.length == 0 ? true : false
+  end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,10 @@ describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
+
+  it 'should check if capacity variable has been set' do
+    expect {DockingStation}.to respond_to(:new).with(1).argument)
+  end
   
   describe '#release_bike' do
     it 'releases working bikes' do
@@ -12,8 +16,9 @@ describe DockingStation do
       #expect(bike).to be_working
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.release_bike).to eq(bike)
+      expect(subject.release_bike).to be_an_instance_of(Bike)
     end
+    
     it 'does not release a bike if none are availble' do
       expect { subject.release_bike }.to raise_error("no availble bikes")
     end
@@ -24,22 +29,19 @@ describe DockingStation do
   it { is_expected.to respond_to :bike }
 
   describe "#dock" do
-    it 'checking bike is docked' do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq(bike)
-    end
-
     it 'raises an error when capacity is reached' do
-      subject.dock(Bike.new)
+      subject.DEAFAULT_CAPACITY.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error("docking station capacity exceeded")
     end  
-  end
+  
+    it 'checking a bike is docked' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike_array.include?(bike)).to eq (true)
+    end
 
-  it 'returns a bike to dock' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq(bike)
-  end
+  
+  end  
 
 end
 
